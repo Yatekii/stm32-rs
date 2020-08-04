4bea3d012e29d8d0370e3648e7ee0a8091a7dcad
Noah Hüsser <yatekii@yatekii.ch>
2020-08-04 21:14:14 +0200

    Initial tries of generating a proper pack of the STM32MP1. Not really fruitful. The generation works but the SVDs are terrible and the patch files somehow wont work

--- scripts/makecrates.py
+++ scripts/makecrates.py
@@ -14,6 +14,7 @@ import glob
 import os.path
 import argparse
 import yaml
+import re
 
 VERSION = "0.11.0"
 SVD2RUST_VERSION = "0.17.0"
@@ -32,7 +33,8 @@ CRATE_DOC_FEATURES = {
     "stm32l4": ["rt", "stm32l4x1", "stm32l4x5"],
     "stm32l5": ["rt", "stm32l562"],
     "stm32g0": ["rt", "stm32g07x", "stm32g030", "stm32g031", "stm32g041", "stm32g081"],
-    "stm32g4": ["rt", "stm32g431", "stm32g441", "stm32g474", "stm32g484"]
+    "stm32g4": ["rt", "stm32g431", "stm32g441", "stm32g474", "stm32g484"],
+    "stm32mp1": ["rt", "stm32mp15xxx"]
 }
 
 CARGO_TOML_TPL = """\
@@ -198,7 +200,7 @@ def main(devices_path, yes):
 
     for path in glob.glob(os.path.join(devices_path, "*.yaml")):
         yamlfile = os.path.basename(path)
-        family = yamlfile[:7]
+        family = re.search('(stm32[a-zA-Z]*\d)', yamlfile).group(1)
         device = os.path.splitext(yamlfile)[0].lower()
         if family not in devices:
             devices[family] = []
